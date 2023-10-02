636bd7149ed0cc89891ff39a31c3497667fe5e26
Brandon Nestor Reis <brandon.nestor.reis@gmail.com>
2023-10-02 17:41:50 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers,string) {
+    const matchResult = drivers.filter((driver) => driver.toUpperCase() === string.toUpperCase())
+    return matchResult
+}
+
+function fuzzyMatch(drivers,string) {
+    const letterResult = drivers.filter((driver) => driver[0] === string[0])
+    return letterResult
+}
+
+function matchName(drivers, string) {
+    const nameResult = drivers.filter((driver) => driver.name === string)
+    return nameResult
+}
